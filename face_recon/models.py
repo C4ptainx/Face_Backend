7fcd95d1b55237f3ed443e656352e10315e3c886
C4ptainx <alejandrotrujano549@gmail.com>
2023-09-22 14:10:33 -0600

    first commit

---
+++ face_recon/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager,AbstractBaseUser
+
+#  Custom User Manager
+class UserManager(BaseUserManager):
+  def create_user(self, name, especialidad, email, tc, password=None, password2=None):
+      """
+      Creates and saves a User with the given email, name, tc and password.
+      """
+      if not email:
+          raise ValueError('User must have an email address')
+
+      user = self.model(
+          email=self.normalize_email(email),
+          name=name,
+          especialidad=especialidad,
+          tc=tc,
+      )
+
+      user.set_password(password)
+      user.save(using=self._db)
+      return user
+
+  def create_superuser(self, email, name, tc, especialidad, password=None):
+      """
+      Creates and saves a superuser with the given email, name, tc and password.
+      """
+      user = self.create_user(
+          email=email,
+          name=name,
+          especialidad=especialidad,
+          tc=tc,
+          password=password,
+      )
+      user.is_admin = True
+      user.save(using=self._db)
+      return user
+
+#  Custom User Model
+class Asistencia(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    valor_escaneado = models.CharField(max_length=200)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Asistencia de {self.user.name} - {self.fecha_registro}'
+
+class User(AbstractBaseUser):
+  name = models.CharField(max_length=200)
+  especialidad = models.CharField(max_length=200)
+# asistencia = models.CharField(max_length=1500, default="[ ]")
+  email = models.EmailField(
+      verbose_name='Email',
+      max_length=255,
+      unique=True,
+  )
+  tc = models.BooleanField()
+  is_active = models.BooleanField(default=True)
+  is_admin = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  objects = UserManager()
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['name', 'tc']
+
+  def __str__(self):
+      return self.email
+
+  def has_perm(self, perm, obj=None):
+      "Does the user have a specific permission?"
+      # Simplest possible answer: Yes, always
+      return self.is_admin
+
+  def has_module_perms(self, app_label):
+      "Does the user have permissions to view the app `app_label`?"
+      # Simplest possible answer: Yes, always
+      return True
+
+  @property
+  def is_staff(self):
+      "Is the user a member of staff?"
+      # Simplest possible answer: All admins are staff
+      return self.is_admin
---
+++ face_recon/views.py
@@ -0,0 +1,97 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from face_recon.serializers import SendPasswordResetEmailSerializer, UserChangePasswordSerializer, UserLoginSerializer, UserPasswordResetSerializer, UserProfileSerializer, UserRegistrationSerializer
+from django.contrib.auth import authenticate
+from face_recon.renderers import UserRenderer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
+from rest_framework import generics
+from face_recon import serializers 
+import json 
+from .serializers import AsistenciaSerializer
+from .models import Asistencia
+
+# Generate Token Manually
+def get_tokens_for_user(user):
+  refresh = RefreshToken.for_user(user)
+  return {
+      'refresh': str(refresh),
+      'access': str(refresh.access_token),
+  }
+
+class UserRegistrationView(APIView):
+  #renderer_classes = [UserRenderer]
+  def post(self, request, format=None):
+    serializer = UserRegistrationSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.save()
+    token = get_tokens_for_user(user)
+    return Response({'token':token, 'msg':'Registration Successful'}, status=status.HTTP_201_CREATED)
+
+class UserLoginView(APIView):
+  #renderer_classes = [UserRenderer]
+  def post(self, request, format=None):
+    serializer = UserLoginSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    email = serializer.data.get('email')
+    password = serializer.data.get('password')
+    user = authenticate(email=email, password=password)
+    if user is not None:
+      token = get_tokens_for_user(user)
+      return Response({'token':token, 'msg':'Login Success'}, status=status.HTTP_200_OK)
+    else:
+      return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
+
+class AsistenciaListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        user = request.user
+        asistencias = Asistencia.objects.filter(user=user)
+        serializer = AsistenciaSerializer(asistencias, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UserProfileView(APIView):
+  #renderer_classes = [UserRenderer]
+  permission_classes = [IsAuthenticated]
+  def get(self, request, format=None):
+    serializer = UserProfileSerializer(request.user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+   
+  def post(self, request, format=None):
+        user = request.user
+        asistencia_data = request.data.get('asistencia')  # Obtener el valor de asistencia desde el cuerpo de la solicitud
+
+        try:
+            # Crear un nuevo registro de asistencia para el usuario actual
+            asistencia = Asistencia.objects.create(user=user, valor_escaneado=asistencia_data)
+
+            serializer = AsistenciaSerializer(asistencia)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+class UserChangePasswordView(APIView):
+ # renderer_classes = [UserRenderer]
+  permission_classes = [IsAuthenticated]
+  def post(self, request, format=None):
+    serializer = UserChangePasswordSerializer(data=request.data, context={'user':request.user})
+    serializer.is_valid(raise_exception=True)
+    return Response({'msg':'Password Changed Successfully'}, status=status.HTTP_200_OK)
+
+class SendPasswordResetEmailView(APIView):
+  #renderer_classes = [UserRenderer]
+  def post(self, request, format=None):
+    serializer = SendPasswordResetEmailSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    return Response({'msg':'Password Reset link send. Please check your Email'}, status=status.HTTP_200_OK)
+
+class UserPasswordResetView(APIView):
+  #renderer_classes = [UserRenderer]
+  def post(self, request, uid, token, format=None):
+    serializer = UserPasswordResetSerializer(data=request.data, context={'uid':uid, 'token':token})
+    serializer.is_valid(raise_exception=True)
+    return Response({'msg':'Password Reset Successfully'}, status=status.HTTP_200_OK)
+
