7173e28243c327e77d57820395aacb58e28289a3
C4ptainx <alejandrotrujano549@gmail.com>
2023-10-25 18:17:11 -0600

    cambios en el settings

--- api/settings.py
+++ api/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-import os, dj_database_url 
+import os, dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,14 +86,10 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'pISdOpbJjqnJRWNrULCp',
-        'HOST': 'containers-us-west-200.railway.app',
-        'PORT':'7113',
-    }
+     'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 # JWT Configuration
