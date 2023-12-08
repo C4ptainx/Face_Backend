1ca3cc3e2b47153dd93c0e612e35ee925651f2d1
C4ptainx <alejandrotrujano549@gmail.com>
2023-12-08 11:45:53 -0600

    update

--- api/settings.py
+++ api/settings.py
@@ -142,7 +142,7 @@ if not DEBUG:
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-BASE_URL = 'https://recon-oc4r.onrender.com'
+BASE_URL = 'http://127.0.0.1:8000'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
